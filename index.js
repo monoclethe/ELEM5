3c5f0899abfa349402783c7b6f696a563e66b143
monoclethe <jwrocksyt@gmail.com>
2024-03-06 18:39:57 -0500

    Version 1.10a

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 document.addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
       combine();
+      updateElemDisp();
     }
 });
 
 
 var playArea = document.getElementById("playArea");
-
+var newElemBuffer = [];
 var elements = {
     0: ["Air", "lightblue"],
-    1: ["Earth", "#743600"],
+    1: ["Earth", "brown"],
     2: ["Fire", "red"],
     3: ["Water", "blue"],
     4: ["Steam", "lightblue"],
@@ -47,7 +48,11 @@ var recipes = {
 
 var discovered = [0, 1, 2, 3];
 
-var selected = [-1, -1]
+var selected = [-1, -1];
+
+function updateElemDisp () {
+    document.getElementById("elemDisp").innerHTML = "(" + discovered.length + "/" + Object.keys(elements).length + ")";
+}
 
 function set1st (id) {
     if (selected[0] === -1) {
@@ -71,18 +76,46 @@ function set2nd (id) {
 function combine () {
     let product = false;
     if (selected[0] !== -1 && selected[1] !== -1) {
-        try {
-            product = recipes[selected[0]][selected[1]];
-            console.log(product);
-            if (!discovered.includes(product)) {
-                document.getElementById("playArea").innerHTML += "<div class=\"elem " + elements[product][1] + "\" id=\"" + product + "\" onclick=\"set1st(id)\" oncontextmenu=\"set2nd(id);return false;\">" + elements[product][0] + "</div>";
-                discovered.push(product);
-            }
-        } catch {
+        product = recipes[selected[0]][selected[1]];
+        if (!discovered.includes(product) && product !== undefined) {
+            discovered.push(product);
+            updateDispElems();
+            newElemBuffer.push(product);
+            updateNewElems();
+        } else if (product === undefined) {
+            //Add a GUI system for this later...
+            //discovered, newElemBuffer, updateDispElems, updateNewElems, elements, recipes
+            let response = prompt("Undiscovered Element/Recipe! Type the name / color of the new element.");
+            response = response.split("/");
             
+            let elemNames = [];
+            for (let i = 0; i < Object.keys(elements).length; i++) {
+                elemNames.push(elements[i][0]);
+            }
+            
+            let elemExists = false;
+            if (elemNames.includes(response[0])) {
+                elemExists = true;
+            }
+            
+            if (!elemExists) {
+                elements[Object.keys(elements).length] = [response[0], response[1]];
+                recipes[Object.keys(elements).length - 1] = {};
+                newElemBuffer.push(Object.keys(elements).length - 1);
+                discovered.push(Object.keys(elements).length - 1);
+            }
+
+            recipes[selected[0]][selected[1]] = Object.keys(elements).length - 1;
+            updateDispElems();
+            updateNewElems();
         }
         
     }
+    let prevSelect = selected;
+    selected = [-1, -1];
+    updateColor(prevSelect[0]);
+    updateColor(prevSelect[1]);
+    updateElemDisp();
 }
 function updateColor (id) {
     if (selected[0] === id && selected[1] === id) {
@@ -95,3 +128,44 @@ function updateColor (id) {
         document.getElementById(id).style = "border: 3px solid " + elements[id][1] + ";";
     }
 }
+function addElemToDoc(id, divid) {
+    document.getElementById(divid).innerHTML += "<div class=\"elem " + elements[id][1] + "\" id=\"" + id + "\" onclick=\"set1st(id)\" oncontextmenu=\"set2nd(id);return false;\">" + elements[id][0] + "</div>";
+}
+function addElemToNew(id, divid) {
+    document.getElementById(divid).innerHTML = "<div class=\"elem " + elements[id][1] + "\" id=\"" + id + "\">" + elements[id][0] + "</div><br>" + document.getElementById(divid).innerHTML;
+}
+function updateDispElems() {
+    let area = document.getElementById("elementArea");
+    area.innerHTML = "";
+    let col = "";
+    let categories = [];
+    let workingID;
+    for (let i = 0; i < discovered.length; i++) {
+        workingID = discovered[i];
+        col = elements[workingID][1]
+        if (!categories.includes(col)) {
+            categories.push(col);
+            document.getElementById("elementArea").innerHTML += "<div id=\"" + col + "\"></div>"
+        }
+        addElemToDoc(workingID, col);
+    }
+}
+function updateNewElems () {
+    try {
+        let len = 16;
+        let workingID;
+        document.getElementById("newElemsList").innerHTML = "";
+        if (newElemBuffer.length < 16) {
+            len = newElemBuffer.length;
+        }
+        for (let i = len - 1; i > -1; i--) {
+            workingID = newElemBuffer[i];
+            document.getElementById("newElemsList").innerHTML += "<div class=\"elem " + elements[workingID][1] + "\">" + elements[workingID][0] + "</div><br>";
+        }
+    } catch(err) {
+        alert(err);
+    }
+}
+updateElemDisp();
+updateDispElems();
+updateNewElems();
--- readme.md
+++ readme.md
@@ -1,7 +1,19 @@
 # ELEM5
-My personal Elemental remake. It's very basic at the moment as it is still in the alpha phase
+My personal Elemental remake. It's very basic at the moment as it is still in the alpha phase.
 
 # Changelog
+## Version 1.10a
+### Features
+- Home button is now active; it brings you back to monoclethe.github.io
+- Elements are now smaller and are sorted by color
+- Added element display in top right of play area
+- New Elems tab now displays the last 10 elements discovered in order.
+- Tool for adding new elements
+### Bugfixes
+- Fixed wrong version format on the footer
+- I can't count, so I put the wrong number of base elements on the previous version. It has been fixed since.
 ## Version 1.00a
 ### Features
 - Rudimentary element creation and combination system
+- 11 Base Elements
+- Order-based Element combination (left click for first ingredient, right click for second. Recipies can vary in the future depending on the order)
